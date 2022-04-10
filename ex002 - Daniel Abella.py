0f966fdd190d11adf548aea7b5df785349113c38
RyandroTavares <ryandrotavaresbr@gmail.com>
2022-04-09 22:00:48 -0300

    Questão-02

---
+++ ex002 - Daniel Abella.py
@@ -0,0 +1,6 @@
+# 02 Realiza a leitura de 1 int e imprime o seu antecessor e o seu sucessor.
+
+numero = int(input('Digite um número: '))
+
+print('O seu sucessor é: {}'.format(numero + 1))
+print('O seu antecessor é: {}'.format(numero - 1))
